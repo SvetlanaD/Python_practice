3ac89bed9c17dd0909715f1a941ab46c55f3b8b8
Svetlana Denezhkina <svetlana.denezhkina@epicsoftwaredev.com>
2021-10-26 09:05:24 +0300

    Day1

--- Day1.py
+++ Day1.py
@@ -7,11 +7,11 @@ username = 'Svetlana Denezhkina'
 now = datetime.datetime.now().strftime('%H')
 if '00' <= now <= '04':
     print(f"Доброй ночи, {username}")
-elif '05' <= now <= '09':
+elif '04' < now <= '09':
     print(f"Доброе утро, {username}")
-elif '10' <= now <= '16':
+elif '9' < now <= '16':
     print(f"Добрый день, {username}")
-elif '17' <= now <= '23':
+elif '16' < now <= '23':
     print(f"Добрый вечер, {username}")
 
 # task 2
@@ -21,9 +21,9 @@ now = response.json()['datetime'][11:13]
 
 if '00' <= now <= '04':
     print(f"Доброй ночи, {username}")
-elif '05' <= now <= '09':
+elif '04' < now <= '09':
     print(f"Доброе утро, {username}")
-elif '10' <= now <= '16':
+elif '9' < now <= '16':
     print(f"Добрый день, {username}")
-elif '17' <= now <= '23':
+elif '16' < now <= '23':
     print(f"Добрый вечер, {username}")
